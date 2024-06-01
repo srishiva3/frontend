f8b7769148a335f037a76a738b5dbdef52b1dd21
srishivajothish <81418992+srishiva3@users.noreply.github.com>
2024-06-01 15:30:11 +0530

    Update jobseeker.service.ts

--- revhire/src/app/Service/jobseeker.service.ts
+++ revhire/src/app/Service/jobseeker.service.ts
@@ -8,7 +8,7 @@ import { Application } from '../Model/application';
   providedIn: 'root'
 })
 export class JobseekerService {
-  base_url = "http://20.242.236.34:8082/";
+  base_url = "https://mywebapp19090931.azurewebsites.net/";
   constructor(private http: HttpClient) {   }
 
   public getJobsByUserId(id:Number):Observable <any>{
