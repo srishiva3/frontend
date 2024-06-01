007f4b3059db02e3dcfbda324d3031753a7b0e50
srishivajothish <81418992+srishiva3@users.noreply.github.com>
2024-06-01 20:28:11 +0530

    Update general.service.ts

--- revhire/src/app/Service/general.service.ts
+++ revhire/src/app/Service/general.service.ts
@@ -9,7 +9,7 @@ import { LoginCredentials } from '../Model/login-credentials';
 })
 export class GeneralService {
 
-  base_url = "https://mywebappforevhire.azurewebsites.net/";
+  base_url = "https://mywebapp19090931.azurewebsites.net/";
   constructor(private http: HttpClient) {   }
 
   public getUser(userId:number):Observable <any>{
